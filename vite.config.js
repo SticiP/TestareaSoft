b41a31e096feb30fc22f91b95a2dd62371decf41
SticiP <stici.pavel2002@gmail.com>
2025-04-17 00:47:37 +0300

    added skote admin panel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',   // sau IP-ul mașinii, dacă accesezi din alt device
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue(),
     ],
 });
